999311b945835dfd67621e6401d66398f3d3897c
pawanphp <milopawan@gmail.com>
2017-11-11 22:33:05 +0530

    Add files via upload
    added for testing purpose

---
+++ callback.js
@@ -0,0 +1,23 @@
+var fs = require("fs");
+
+fs.readFile('pawan.txt', function (err, data) {
+	console.log("pawan kumar");
+   if (err) return console.error(err);
+   console.log(data.toString());
+});
+console.log("Program Ended");
+console.log("Going to write into existing file");
+fs.appendFile('pawan.txt', 'Simply Easy Learning!',  function(err) {
+   if (err) {
+      return console.error(err);
+   }
+   
+   console.log("Data written successfully!");
+   console.log("Let's read newly written data");
+   fs.readFile('pawan.txt', function (err, data) {
+      if (err) {
+         return console.error(err);
+      }
+      console.log("Asynchronous read: " + data.toString());
+   });
+});
